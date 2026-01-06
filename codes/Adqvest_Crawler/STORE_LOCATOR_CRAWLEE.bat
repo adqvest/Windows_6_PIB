8477c2aab66f16a32389e15bd8090436fe525db7
pushkarthurro <pushkar.patil@thurro.com>
2026-01-06 17:36:04 +0530

    Delete codes directory

--- codes/Adqvest_Crawler/STORE_LOCATOR_CRAWLEE.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\Administrator\AdQvestDir\Adqvest_Crawler
-python Store_Runner_V2.py
-pause
--- codes/Adqvest_Crawler/STORE_LOCATOR_CRAWLEE_REEBOK.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\Administrator\AdQvestDir\Adqvest_Crawler
-python A001_STORE_LOCATOR_CRAWLEE_REEBOK.py
-pause
